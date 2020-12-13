e151082dc8f6088e7222bd793a8b7bf81037968a
Terrii <minimatt10@gmail.com>
2020-12-14 00:37:38 +1030

    More clans page work and search

--- src/pages/Clans.js
+++ src/pages/Clans.js
@@ -1,8 +1,8 @@
 import React, { Component, useRef, useEffect } from 'react';
 import ReactTooltip from 'react-tooltip';
+import { findBestMatch } from 'string-similarity';
 import Error from '../modules/Error';
 import Loader from '../modules/Loader';
-import Search from '../modules/Search';
 import ClanBannerGenerator from '../modules/ClanBanner';
 import { formatSmallTime } from '../modules/Misc';
 import * as apiRequest from '../modules/requests/API';
@@ -15,7 +15,9 @@ export class Clans extends Component {
       statusText: `Collecting clan data...`,
       loading: true
     },
-    pageNumber: 0
+    pageNumber: 0,
+    input: "",
+    results: []
   }
 
   componentDidMount() {
@@ -41,27 +43,48 @@ export class Clans extends Component {
     return e.childNodes.length === 0 ? "" : e.childNodes[0].nodeValue;
   }
 
+  searchForClans = (input) => {
+    if(input === "") { this.setState({ input: input, results: [] }); }
+    else { this.setState({ input: input, results: findBestMatch(input, this.state.clans.map(e => e.clanName)).ratings.filter(e => e.rating > 0.2).sort((a,b) => b.rating - a.rating) }); }
+  }
+
   render() {
     const { status, statusText } = this.state.status;
     if(status === "error") { return (<Error statusText={ statusText } />) }
     else if(status === "ready") {
       return (
         <div className="page-content" style={{ overflow: "hidden" }}>
-          <Search />
-          <div className="clans-container transScrollbar">
-            {
-              this.state.clans.slice(0, 45).map(clan => {
-                return (
-                  <div className={`clan-container ${ clan.isTracking ? "tracking" : "not-tracking" }`}>
-                    <div className="clan-banner-icon-container"><ClanBannerGenerator clanID={ clan.clanID } clanBanner={ clan.clanBanner } /></div>
-                    <div className="clan-details">
-                      <div className="clan-name">{ this.htmlDecode(clan.clanName) } ({ this.htmlDecode(clan.clanCallsign) })</div>
-                      <div className="clan-members">{ clan.memberCount } / 100</div>
+          <div className="clans-search-container">
+            <input className="clans-searchbar" type="text" placeholder="Search..." aria-label="Search..." data-lpignore="true" onKeyUp={ event => { this.searchForClans(event.target.value) } } />
+          </div>
+          <div className="clans-content">
+            <div className="clans-container transScrollbar">
+              {
+                this.state.input.length > 0 ? this.state.results.map(clanTarget => {
+                  let clan = this.state.clans.find(e => clanTarget.target === e.clanName);
+                  return (
+                    <div className={`clan-container ${ clan.isTracking ? "tracking" : "not-tracking" }`}>
+                      <div className="clan-banner-icon-container"><ClanBannerGenerator clanID={ clan.clanID } clanBanner={ clan.clanBanner } /></div>
+                      <div className="clan-details">
+                        <div className="clan-name">{ this.htmlDecode(clan.clanName) } ({ this.htmlDecode(clan.clanCallsign) })</div>
+                        <div className="clan-members">{ clan.memberCount } / 100</div>
+                      </div>
                     </div>
-                  </div>
-                );
-              })
-            }
+                  )
+                }) : (
+                this.state.clans.slice(0, 45).map(clan => {
+                  return (
+                    <div className={`clan-container ${ clan.isTracking ? "tracking" : "not-tracking" }`}>
+                      <div className="clan-banner-icon-container"><ClanBannerGenerator clanID={ clan.clanID } clanBanner={ clan.clanBanner } /></div>
+                      <div className="clan-details">
+                        <div className="clan-name">{ this.htmlDecode(clan.clanName) } ({ this.htmlDecode(clan.clanCallsign) })</div>
+                        <div className="clan-members">{ clan.memberCount } / 100</div>
+                      </div>
+                    </div>
+                  );
+                }))
+              }
+            </div>
             <div className="clans-footer">
               <div>{`<`} Page: { this.state.pageNumber } {`>`}</div>
             </div>
